5a23702cd0bda29710ef62187efae8464dd5a19a
prafullx24 <prafullx24@gmail.com>
2025-02-18 14:51:05 +0530

    prompt experiments

--- extract_data.py
+++ extract_data.py
@@ -134,17 +134,57 @@ def insert_instrument_type(user_id, project_id, file_id, instrument_type):
         return None, str(e)
 
 
+def insert_extracted_data(user_id, project_id, file_id, instrument_type):
+
+    # To Do:
+        """
+        Add all values from Runsheet Propmt Columns and match them with extracted_data column. 
+        """
+    try:
+        conn = get_db_connection()
+        if conn is None:
+            return None, "Database connection error"
+
+        with conn:
+            with conn.cursor() as cur:
+                query = """
+                INSERT INTO public.extracted_data (user_id, file_id, project_id, instrument_type)
+                VALUES (%s, %s, %s, %s::jsonb)
+                ON CONFLICT (file_id) 
+                DO UPDATE 
+                SET instrument_type = EXCLUDED.instrument_type
+                RETURNING id;
+                """
+                user_id = int(user_id)
+                file_id = int(file_id)  # Convert to integer
+                project_id = int(project_id)  # Convert to integer
+                instrument_type_json = json.dumps(instrument_type)
+                try:
+                    cur.execute(query, (user_id, file_id, project_id, instrument_type_json))
+                    extracted_data_id = cur.fetchone()[0]
+                except Exception as e:
+                    print("Error inserting into database:", e)
+                    return None, "Database insert failed"
+
+                conn.commit()
+                return extracted_data_id, None
+
+    except Exception as e:
+        print(f"Error Inserting instrument_type: {e}")
+        return None, str(e)
+
+
 
 # API Endpoint to fetch OCR text (file_id passed in URL as part of the route)
 
 def extract_instrument_type(ocr_text):
     client = OpenAI()
     # Define system and user prompts
-    system_prompt = """
+    system_prompt = f"""
     You are a legal expert extraction algorithm specializing in property law and land transactions.
     Extract the following details from the provided legal land document and provide output in valid JSON format.
     """
-    user_prompt_doc_type = """Extract legal information from the following document:\n\n{ocr_text}. 
+    user_prompt_doc_type = f"""Extract instrument type from the following document:\n\n{ocr_text}. 
     Instrument Type can be one of following: Deed, Lease, Release, Waiver, Quitclaim, Option, Easement or Right of Way, Ratification, Affidavit, Probate, Will and Testament, Death Certificate, Obituary, Divorce, Adoption, Court Case, Assignment or Other. If the type is an amendment, return what kind of instrument it is amending. 
     Supporting evidence should be from the text provided as document in this prompt. 
     Expected output json should contain following fields: instrument type and supporting evidence from the input text data. In output json, keep two fields, one string "instrument_type" and second array of strings "supportingEvidence"."""
@@ -160,7 +200,71 @@ def extract_instrument_type(ocr_text):
     # Print extracted data
     return completion.choices[0].message.content
 
+def extract_volume_page_document_case_number(ocr_text):
+    client = OpenAI()
+    # Define system and user prompts
+    system_prompt = f"""
+    You are a legal expert extraction algorithm specializing in property law and land transactions.
+    Extract the following details from the provided legal land document and provide output in valid JSON format.
+    """
+
+    user_prompt_doc_type = f"""Extract Volume number or Page Number and Document or Case Number from the following document:\n\n{ocr_text}. 
+    Volume number or Page Number: Reference the recording information found in the document and/or.the name of the file to determine the volume number and page number of where the document was filed with the county. Often the name of the file includes the recording information written simply as a 1 to 4 digit numbers with a dash or space between them. Return as "Volume number/Page Number".
+    Document or Case Number: Reference information found in the margins, top of the document, or in the file name, to determine the case, file, or recording number. Return as "#" followed by the number. Not all documents have a number like this, so if none is found, return "none found".
+    """
+
+    # Send request to OpenAI
+    completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt_doc_type}
+    ]
+    )
+    print(user_prompt_doc_type)
+    print(completion.choices[0].message)
+    # Print extracted data
+    return completion.choices[0].message.content
+
+
+"""
+Execution Date
+Effective Date
+Recording Date
+Grantor(s) - ***Note that Grator(s) and Grantee(s) need to indicate when things are owned jointly. Use {name1, name2, name3} for things owned jointly, and the list of names without brackets when owned individually.
+Grantee(s) - ***Same note as Grantor(s)
+Property Description
+"""
+def extract_volume_page_document_case_number(ocr_text):
+    client = OpenAI()
+    # Define system and user prompts
+    system_prompt = f"""
+    You are a legal expert extraction algorithm specializing in property law and land transactions.
+    Extract the following details from the provided legal land document and provide output in valid JSON format.
+    """
+
+    user_prompt_doc_type = f"""Extract Volume number or Page Number and Document or Case Number from the following document:\n\n{ocr_text}. 
+    Volume number or Page Number: Reference the recording information found in the document and/or.the name of the file to determine the volume number and page number of where the document was filed with the county. Often the name of the file includes the recording information written simply as a 1 to 4 digit numbers with a dash or space between them. Return as "Volume number/Page Number".
+    Document or Case Number: Reference information found in the margins, top of the document, or in the file name, to determine the case, file, or recording number. Return as "#" followed by the number. Not all documents have a number like this, so if none is found, return "none found".
+    """
+
+    # Send request to OpenAI
+    completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt_doc_type}
+    ]
+    )
+    print(user_prompt_doc_type)
+    print(completion.choices[0].message)
+    # Print extracted data
+    return completion.choices[0].message.content
+
+
 def find_confidence(ocr_data, instrument_data):
+
+    # Incomplete Function DO NOT CALL
     best_match = None
     highest_confidence = 0
 
@@ -177,6 +281,8 @@ def find_confidence(ocr_data, instrument_data):
 
 
 
+
+
 # Flask App Factory
 def create_app():
     app = Flask(__name__)
@@ -190,16 +296,14 @@ def create_app():
 
             else:
                 ocr_text = ocr_data.get("text", "")
-                print(ocr_text)
-                extracted_data = extract_instrument_type(ocr_text)
+                # print(ocr_text)
+
+                extracted_instrument_type = extract_instrument_type(ocr_text)
+                cleaned_extracted_instrument_type = extracted_instrument_type.strip("```").lstrip("json\n").strip()
                 
-                cleaned_response = extracted_data.strip("```").lstrip("json\n").strip()
-                # cleaned_response = cleaned_response.replace("\n", "").replace(r'\"', "")
-
-                # print(ocr_data)
-                # print(cleaned_response)
-
+                
                 extracted_data_id = insert_instrument_type(user_id, project_id, file_id, cleaned_response)
+
 
 
 
@@ -212,5 +316,11 @@ def create_app():
 
 # Run the Flask application
 if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
+    # app = create_app()
+    # app.run(debug=True)
+    file_id, project_id, ocr_data, error = fetch_ocr_text(63)
+    # print(ocr_data)
+    ocr_text = ocr_data.get("text", "")
+    print(ocr_text)
+    print(extract_volume_page_document_case_number(ocr_text))
+
